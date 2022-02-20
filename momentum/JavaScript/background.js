a4b48ea11a4172db9c1e7dae0b1b67afd2fa68ec
Json604Park <99090155+Json604Park@users.noreply.github.com>
2022-02-21 00:43:35 +0900

    Delete momentum directory

--- momentum/JavaScript/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["b_img1.jpg", "b_img2.jpg"];
-
-const currImg = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `Image/${currImg}`;
-
-document.body.appendChild(bgImage);
